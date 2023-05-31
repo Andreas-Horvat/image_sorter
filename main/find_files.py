8bace03e175f40eb34042bee3e330baefabaf1fb
Andreas Horvat <37000853+Andreas-Horvat@users.noreply.github.com>
2023-05-31 16:22:52 +0200

    correct docstrings find_files.py

--- main/find_files.py
+++ main/find_files.py
@@ -5,9 +5,9 @@ from typing import List
 
 def find_files_in_directory(path: str) -> List[str]:
     """
-    Returned rekursiv alle in dem Direktory gefundenen Bilder
-    :param path: Directory das rekursiv nach bildern durchsucht werden soll
-    :return: alle Bilder die in dem Directory geunden wurden
+    Returns recursively all images found in the directory
+    :param path: path to directory
+    :return: list of all paths to the images found in the directory
     """
     all_image_paths = []
     for root, dirs, files in os.walk(path):
